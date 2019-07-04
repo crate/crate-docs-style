448ee7f704a795628743a4860b9da6f4e5b775a0
Naomi Slater <nslater@tumbolia.org>
2019-07-04 17:30:30 +0200

    Fix minor consistency issues

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+# from crate.theme.rtd.conf.foo import *
+
 source_suffix = '.rst'
 
 exclude_patterns = ['.*', '*.lint']
@@ -5,3 +7,4 @@ exclude_patterns = ['.*', '*.lint']
 master_doc = 'index'
 
 extensions = ['sphinx_sitemap']
+site_url = 'https://crate-docs-style.readthedocs.io/en/latest/'
